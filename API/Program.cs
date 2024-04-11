73fbbe1b51194348909f050a249145c7a56ee7c5
Adam M <whiskypo082@gmail.com>
2024-04-11 08:14:13 -0500

    Added API side of Identity.

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,18 @@
-using API.Errors;
 using API.Extensions;
 using API.MIddleware;
-using Core.Interfaces;
+using Core.Entities.Identity;
 using Infrastructure.Data;
-using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Data.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddApplicationServices(builder.Configuration); 
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration); 
 
 var app = builder.Build();
 
@@ -29,6 +31,7 @@ app.UseStaticFiles();
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -36,12 +39,16 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider; 
 var context = services.GetRequiredService<StoreContext>(); 
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>(); 
 
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context); 
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
 catch (Exception ex)
 {
